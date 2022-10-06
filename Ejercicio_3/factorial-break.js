f93a255b779de2f7e208264cc73df3be5eb97da6
Danielfmv <44736460+Danielfmv@users.noreply.github.com>
2022-10-06 16:40:08 -0500

    Add files via upload

---
+++ Ejercicio_3/factorial-break.js
@@ -0,0 +1,16 @@
+// Este archivo debe calcular el factorial de 10 utilizando un bucle while, una bifurcación if y una sentencia break
+
+let i = 1;
+let factorial = 10;
+let resultado = 1;
+
+while (true) {
+    i++
+    resultado = resultado * i
+    if (i >= factorial) {
+        break;
+    }
+    
+}
+
+console.log(resultado)
