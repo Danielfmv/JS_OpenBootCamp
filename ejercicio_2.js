4d88566758698b1d6a6d11414042f3093d949abd
Danielfmv <44736460+Danielfmv@users.noreply.github.com>
2022-10-06 10:20:50 -0500

    Create ejercicio_2.js

---
+++ ejercicio_2.js
@@ -0,0 +1,23 @@
+//NOMBRE
+const miNombre = "Daniel Felipe";
+
+
+//EDAD
+let edad = 27;
+
+
+//PREGUNTA
+const eres_desarrollador = true;
+
+
+//FECHA DE NACIMIENTO
+const nacimiento = new Date(1995, 1, 24)
+
+
+//LIBRO FAVORITO
+const libro_favorito = {
+    titulo: "El lobo estepario",
+    autor: "Herman Hesse",
+    fecha: 1927,
+    url: "https://www.amazon.com/-/es/Hermann-hesse/dp/154514351X"
+}
