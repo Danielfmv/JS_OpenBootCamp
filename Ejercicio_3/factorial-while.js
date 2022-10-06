e53ca13ee31caebb7a5f847f7074286cd5fc2e82
Danielfmv <44736460+Danielfmv@users.noreply.github.com>
2022-10-06 16:39:39 -0500

    Create factorial-while.js

---
+++ Ejercicio_3/factorial-while.js
@@ -0,0 +1,11 @@
+// Este archivo debe calcular el factorial de 10 utilizando un bucle while
+
+let i = 1;
+let max = 10;
+let resultado = 1
+
+while (i <= max) {
+    i++
+    resultado = resultado * i;
+    console.log(resultado)
+}
