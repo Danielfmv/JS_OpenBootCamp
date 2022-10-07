bc898ca03be466bb37cf2cd1ac495b01ad3eebcb
Danielfmv <44736460+Danielfmv@users.noreply.github.com>
2022-10-07 10:29:14 -0500

    Add files via upload

---
+++ ejercicio_5.js
@@ -0,0 +1,33 @@
+// Una variable que contenga tu altura en centímetros (entero)
+
+let altura_cm = 178;
+
+// Una variable que contenga tu altura en metros (número de coma flotante)
+
+let altura_m = 1.78;
+
+// Una variable que contenga tu peso en kilogramos (número de coma flotante)
+
+let peso_kg = 75.3;
+
+// Una variable que contenga tu altura en metros redondeada hacia arriba
+
+let altura_redo = Math.ceil(altura_m)
+
+// Una variable que contenga tu peso en kilogramos redondeado hacia abajo
+
+let peso_redo = Math.round(peso_kg);
+
+// Una variable que contenga si "el máximo valor que se puede obtener en Javascript + 1" es igual al máximo valor que se puede obtener en Javascript
+
+let max_val = Number.MAX_VALUE;
+
+let max_val_masUno = max_val + 1
+
+//console.log(max_val === max_val_masUno) // --> true
+
+let maxVal_igual = console.log(max_val === max_val_masUno) // --> true
+
+
+
+
