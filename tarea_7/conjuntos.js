75817854aa3c907a4900a9e0882d1ebb3c2d3626
Danielfmv <44736460+Danielfmv@users.noreply.github.com>
2022-10-07 18:12:38 -0500

    Add files via upload

---
+++ tarea_7/conjuntos.js
@@ -0,0 +1,38 @@
+// Un nuevo Set con los nombres de tu familia
+
+const array = ["Ana", "Natalia", "Mitch", "Juan", "Sára", "Alonso", "Daniel"]
+
+const familia = new Set(array);
+
+// Modifica el Set original añadiendo tu nombre (duplicado) (debería darte lo mismo)
+
+familia.add("Daniel");
+console.log(familia)
+
+// Modifica el Set original añadiendo el nombre "Javascript" (ya que empieza a formar parte de tu vida ;)
+
+familia.add("Javascript")
+console.log(familia)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
